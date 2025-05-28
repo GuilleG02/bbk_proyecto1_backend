5bb00efd09e6c1338a6b07368ab5b79549df4cdb
GuilleG02 <guille.g0220rgmail.com>
2025-05-28 19:58:45 +0200

    Arreglo del diagrama del DBeaver

--- migrations/20250528164224-create-order-product.js
+++ migrations/20250528164224-create-order-product.js
@@ -10,10 +10,22 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       order_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Orders",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       product_id: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Products",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
       },
       createdAt: {
         allowNull: false,
