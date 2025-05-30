68e81f82a7dbc111f210bbc9db3f5ed6956c1380
GuilleG02 <guille.g0220rgmail.com>
2025-05-30 18:59:38 +0200

    Arreglo de las URL

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+app.use('/product', require('./routes/products'));
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
