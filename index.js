f07824b817c0d53925a297c0de4babc2df36e6ad
GuilleG02 <guille.g0220rgmail.com>
2025-07-14 19:30:13 +0200

    Cambio de puerto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 const { typeError } = require("./middlewares/errors");
 
 app.use(express.json());
